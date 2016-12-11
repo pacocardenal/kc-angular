e055a3e0808f78ca40b80a6375b2030ebfcd1708
Paco Cardenal <paco.cardenal@talentomobile.com>
2016-12-11 20:32:02 +0100

    NG2 - Creamos nuestro primer componente

--- agenda/app/app.module.ts
+++ agenda/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  // Usamos 'imports' para importar otros módulos
+  // de los cuales dependemos
+  imports:      [ 
+    BrowserModule 
+  ],
+  // En declaraciones añadimos todos los componentes,
+  // pipes y directivas de nuestra aplicación
+  declarations: [ 
+    AppComponent,
+    ListaContactosComponent 
+  ],
+  // En 'providers' añadimos todos los servicios de
+  // nuestra aplicación
+  providers: [],
+  bootstrap:    [ 
+    AppComponent 
+  ]
 })
 export class AppModule { }
