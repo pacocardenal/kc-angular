83b16e93246fcd24068b12dfa9dc98c18e5087f0
Paco Cardenal <paco.cardenal@talentomobile.com>
2016-12-11 21:13:03 +0100

    NG2 - Hooks

--- agenda/app/lista-contactos/lista-contactos.component.ts
+++ agenda/app/lista-contactos/lista-contactos.component.ts
@@ -1,14 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "lista-contactos",
     templateUrl: "./app/lista-contactos/lista-contactos.component.html"
 })
 
-export class ListaContactosComponent {
-   contactos : string[] = [
-     "Tim Cook",
-     "Bill Gates",
-     "Elon Musk"
-   ]; 
+export class ListaContactosComponent implements OnInit {
+
+  contactos: string[];
+
+  // El método 'ngOnInit' viene dado por la interfaz "OnInit"
+  // que es el hook en el cual inicializamos los valores del
+  // componente
+  ngOnInit(): void {
+    this.contactos = [
+      "Tim Cook",
+      "Bill Gates",
+      "Elon Musk"
+    ]; 
+  }
+
 }
